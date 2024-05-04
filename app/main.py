c4bfc714f9b2eb3bc22f92f0a6dbec51a950a8e4
Azim Hatami <azimhatami4@gmail.com>
2024-05-04 13:26:45 +0330

    Fix: 'ObjectId' is not iterable error

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from pymongo import MongoClient
 from bson.objectid import ObjectId
@@ -11,52 +11,83 @@ client = MongoClient('mongodb://todo_fastapi-mongo-1:27017')
 db = client['todo_db']
 collection = db['todo_collection']
 
+
 # ToDo model
 class ToDo(BaseModel):
     title: str
     description: str
 
 
-@app.post('/todos')
+@app.post('/todos/')
 async def create_todo(todo: ToDo):
-    result = collection.insert_one(todo.dict())
+    '''
+    Create a new ToDo item.
+    '''
+    todo_data = {
+        'title': todo.title,
+        'description': todo.description,
+    }
+    result = collection.insert_one(todo_data)
     return {
         '_id': str(result.inserted_id),
         'title': todo.title,
         'description': todo.description,
     }
 
-@app.get('/todos')
+@app.get('/todos/')
 async def get_todos():
+    '''
+    Get all ToDo items.
+    '''
     todos = []
     for todo in collection.find():
-        todos.append(todo)
+        todos.append({
+            '_id': str(todo['_id']),
+            'title': todo['title'],
+            'description': todo['description']
+        })
     return todos
 
 
-@app.get('/todos/{todo_id}')
+@app.get('/todos/{todo_id}/')
 async def get_todo(todo_id: str):
+    '''
+    Get a specific ToDo item by ID.
+    '''
+    if not ObjectId.is_valid(todo_id):
+        raise HTTPException(status_code=400, detail='Invalid todo_id')
     todo = collection.find_one({'_id': ObjectId(todo_id)})
     if todo:
-        return todo
+        return {
+            '_id': str(todo['_id']),
+            'title': todo['title'],
+            'description': todo['description']
+        }
     else:
-        return {'message': 'ToDo not found'}
+        raise HTTPException(status_code=404, detail='ToDo Not found')
 
-@app.put('/todos/{todo_id}')
+@app.put('/todos/update/{todo_id}/')
 async def update_todo(todo_id: str, todo: ToDo):
-    result = collection.update_one(
-        {'_id': ObjectId(todo_id)},
-        {'$set': todo.dict()},
-    )
-    if result.modified_count == 1:
-        return {'message': 'ToDo updated successfully'}
-    else:
-        return {'message': 'ToDo not found'}
+    '''
+    Update a ToDo item by ID.
+    '''
+    existing_todo = collection.find_one({'_id': ObjectId(todo_id)})
+    if existing_todo:
+        result = collection.update_one(
+            {'_id': ObjectId(todo_id)},
+            {'$set': todo.dict()},
+        )
+        if result.modified_count == 1:
+            raise HTTPException(status_code=200, detail='ToDo updated successfully')
+    raise HTTPException(status_code=404, detail='ToDo Not found')
 
-@app.delete('/todos/{todo_id}')
+@app.delete('/todos/delete/{todo_id}')
 async def delete_todo(todo_id: str):
+    '''
+    Delete a ToDo item by ID.
+    '''
     result = collection.delete_one({'_id': ObjectId(todo_id)})
     if result.deleted_count == 1:
-        return {'message': 'ToDo deleted successfully'}
+        raise HTTPException(status_code=200, detail='ToDo deleted successfully')
     else:
-        return {'message': 'ToDo not found'}
+        raise HTTPException(status_code=404, detail='ToDo Not found')
