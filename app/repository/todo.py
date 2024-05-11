69cbe35e5840e98c6a9d158d81ea61a5b637937e
Azim Hatami <azimhatami4@gmail.com>
2024-05-11 10:22:22 +0330

    Fix: TypeError object is not callable

--- app/repository/todo.py
+++ app/repository/todo.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from bson.objectid import ObjectId
 
 from app.schemas.todo import ToDo
@@ -10,60 +9,71 @@ class ToDoRepository:
     def __init__(self, collection):
         self.collection = collection
 
-    async def __call__(self, method, *args, **kwargs):
-        if method == 'create_todo':
-            return await self.create_todo(*args, **kwargs)
-        elif method == 'get_all_todos':
-            return await self.get_all_todos(*args, **kwargs)
-        elif method == 'get_todo_by_id':
-            return await self.get_todo_by_id(*args, **kwargs)
-        elif method == 'update_todo':
-            return await self.update_todo(*args, **kwargs)
-        elif method == 'delete_todo':
-            return await self.delete_todo(*args, **kwargs)
-
-    async def create_todo(self, todo: ToDo) -> ToDo:
+    def create_todo(self, todo: ToDo) -> ToDo:
+        """
+        Create a new ToDo item.
+        """
         todo_data = todo.dict()
         result = self.collection.insert_one(todo_data)
-        inserted_todo = await self.collection.find_one({'_id': result.inserted_id})
-        return ToDo(**inserted_todo)
+        inserted_todo = self.collection.find_one({'_id': result.inserted_id})
+        return {
+            '_id': str(inserted_todo['_id']),
+            'title': todo.title,
+            'description': todo.description
+        }
 
-    async def get_all_todos(self) -> list[ToDo]:
+    def get_all_todos(self) -> list[ToDo]:
+        """
+        Get all ToDo items.
+        """
         todos = []
         for todo in self.collection.find():
-            todos.append(ToDo(**todo))
+            todos.append({
+                '_id': str(todo['_id']),
+                'title': todo['title'],
+                'description': todo['description']
+            })
         return todos
 
-    async def get_todo_by_id(self, todo_id: str) -> ToDo:
+    def get_todo_by_id(self, todo_id: str) -> ToDo:
+        """
+        Get a specific ToDo item by ID.
+        """
         if not ObjectId.is_valid(todo_id):
             raise HTTPException(status_code=400, detail='Invalid todo_id')
-        todo = await self.collection.find_one({'_id': ObjectId(todo_id)})
+        todo = self.collection.find_one({'_id': ObjectId(todo_id)})
         if not todo:
             raise HTTPException(status_code=400, detail='Invalid todo_id')
         return ToDo(**todo)
 
-    async def update_todo(self, todo_id: str, todo: ToDo) -> ToDo:
+    def update_todo(self, todo_id: str, todo: ToDo) -> ToDo:
+        """
+        Update a ToDo item by ID.
+        """
         if not ObjectId.is_valid(todo_id):
             raise HTTPException(status_code=400, detail='Invalid todo_id')
 
-        existing_todo = await self.collection.find_one({'_id': ObjectId(todo_id)})
+        existing_todo = self.collection.find_one({'_id': ObjectId(todo_id)})
         if not existing_todo:
             raise HTTPException(status_code=404, detail='ToDo Not found')
 
-        update_result = await self.collection.update_one(
+        update_result = self.collection.update_one(
             {'_id': ObjectId(todo_id)},
             {'$set': todo.dict()}
         )
-        if udate_result.modified_count != 1:
-            raise HTTPException(status_code=500, detail='Failed to update ToDo')
+        if update_result.modified_count == 1:
+            raise HTTPException(status_code=200, detail='ToDo update successfully')
 
-        updated_todo = await self.collection.find_one({'_id': ObjectId(todo_id)})
+        updated_todo = self.collection.find_one({'_id': ObjectId(todo_id)})
         return ToDo(**updated_todo)
 
-    async def delete_todo(self, todo_id: str) -> None:
-        if not ObjectId.isvalid(todo_id):
+    def delete_todo(self, todo_id: str) -> None:
+        """
+        Delete a ToDo item by ID.
+        """
+        if not ObjectId.is_valid(todo_id):
             raise HTTPException(status_code=400, detail='Invalid todo_id')
-        delete_result = await self.collection.delete_one(
+        delete_result = self.collection.delete_one(
             {'_id': ObjectId(todo_id)}
         )
         if delete_result.deleted_count != 1:
--- app/routers/todo.py
+++ app/routers/todo.py
@@ -1,65 +1,32 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
 from app.repository.todo import ToDoRepository
 from app.data_adapter.db import collection
 from app.schemas.todo import ToDo
 
 
-todo = APIRouter()
+todo_router = APIRouter()
 
-# Dependency injection for cleaner API routes
 todo_repository = ToDoRepository(collection)
 
-@todo.post('/todos/')
+@todo_router.post('/todos/')
 async def create_todo(
     todo: ToDo,
-    todo_repo: ToDoRepository = Depends(todo_repository)
 ):
-    '''
-    Create a new ToDo item.
-    '''
-    print('Routers Ok')
-    return todo_repo('create_todo', todo)
+    return todo_repository.create_todo(todo)
 
-@todo.get('/todos/')
-async def get_todos(todo_repo: ToDoRepository = Depends(todo_repository)):
-    '''
-    Get all ToDo items.
-    '''
-    print('Routers Ok')
-    return todo_repo('get_all_todos')
+@todo_router.get('/todos/',)
+async def get_todos():
+    return todo_repository.get_all_todos()
 
-@todo.get('/todos/{todo_id}')
-async def get_todo(
-    todo_id: str,
-    todo_repo: ToDoRepository = Depends(todo_repository)
-):
-    '''
-    Get a specific ToDo item by ID.
-    '''
-    print('Routers Ok')
-    return todo_repo('get_todo_by_id', todo_id)
+@todo_router.get('/todos/{todo_id}', response_model=ToDo)
+async def get_todo(todo_id: str):
+    return todo_repository.get_todo_by_id(todo_id)
 
-@todo.put('/todos/update/{todo_id}')
-async def update_todo(
-    todo_id: str,
-    todo: ToDo,
-    todo_repo: ToDoRepository = Depends(todo_repository)
-):
-    '''
-    update a ToDo item by ID.
-    '''
-    print('Routers Ok')
-    return todo_repo('update_todo', todo_id, todo)
+@todo_router.put('/todos/update/{todo_id}', response_model=list[ToDo])
+async def update_todo(todo_id: str, todo: ToDo):
+    return todo_repository.update_todo(todo_id, todo)
 
-@todo.delete('/todos/delete/{todo_id}')
-async def delete_todo(
-    todo_id: str,
-    todo_repo: ToDoRepository = Depends(todo_repository)
-):
-    '''
-    Delete a ToDo item by ID.
-    '''
-    print('Routers Ok')
-    return todo_repo('delete_todo', todo_id)
-
+@todo_router.delete('/todos/delete/{todo_id}')
+async def delete_todo(todo_id: str):
+    return todo_repository.delete_todo(todo_id)
