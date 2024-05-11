c2306dc9756b86811ae35ca2b13d49b52ea20fd8
Azim Hatami <azimhatami4@gmail.com>
2024-05-11 10:20:10 +0330

    Add root page

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-from app.routers.todo import todo
+from app.routers.todo import todo_router
 
 
 # App object
 app = FastAPI()
-app.include_router(todo)
+app.include_router(todo_router)
+
+@app.get('/')
+def root():
+    return HTMLResponse('<center><h1>Welcome to fastAPI.</h1></center>')
